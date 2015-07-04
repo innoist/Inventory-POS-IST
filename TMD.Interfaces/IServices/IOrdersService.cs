aaa5024a227c933fde920f9f4a8f19bea223b8b0
Syed Usman <usman@innostark.com>
2015-07-04 12:49:00 +0500

    Interfacecommitted

---
+++ TMD.Interfaces/IServices/IOrdersService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using TMD.Models.DomainModels;
+
+namespace TMD.Interfaces.IServices
+{
+    public interface IOrdersService
+    {
+        Order GetOrders(long orderId);
+        IEnumerable<Order> GetAllOrders();
+    }
+}
