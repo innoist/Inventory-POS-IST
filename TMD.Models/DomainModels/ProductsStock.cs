c2894ccb4e64831c975c6ad825dbb7614b9b9f27
curiousnadir <nadir@innostark.com>
2015-07-14 15:53:25 +0500

    ProductsStock View Model

---
+++ TMD.Models/DomainModels/ProductsStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMD.Models.DomainModels
+{
+    public class ProductsStock
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SalePrice { get; set; }
+        public long? PurchasedQty { get; set; }
+        public int? SoldQty { get; set; }
+        public long AvailableQty { get; set; }
+    }
+}
